fad55591b1de953a16012253666c3ecb5fe9e8fd
Shane Kelly <skelly1@lappy-14.04>
2017-04-04 00:05:33 -0400

    ADD: exercise 07

--- exercises/ex07/hash.c
+++ exercises/ex07/hash.c
@@ -179,8 +179,7 @@ int hash_hashable(Hashable *hashable)
  */
 int equal_int (void *ip, void *jp)
 {
-    // FILL THIS IN!
-    return 0;
+    return *(int*)ip == *(int*)jp;
 }
 
 
@@ -193,8 +192,7 @@ int equal_int (void *ip, void *jp)
  */
 int equal_string (void *s1, void *s2)
 {
-    // FILL THIS IN!
-    return 0;
+    return *(char*)s1 == *(char*)s2;
 }
 
 
@@ -208,8 +206,7 @@ int equal_string (void *s1, void *s2)
  */
 int equal_hashable(Hashable *h1, Hashable *h2)
 {
-    // FILL THIS IN!
-    return 0;
+    return h1->equal(h1->key, h2->key);
 }
 
 
@@ -295,10 +292,19 @@ Node *prepend(Hashable *key, Value *value, Node *rest)
 
 
 /* Looks up a key and returns the corresponding value, or NULL */
-Value *list_lookup(Node *list, Hashable *key)
-{
-    // FILL THIS IN!
-    return NULL;
+Value *list_lookup(Node *list, Hashable *key) {
+    if (list == NULL) {
+        return NULL;
+    }
+    else if (equal_hashable(list->key, key)){
+        return list->value;
+    }
+    else if (list->next == NULL){
+        return NULL;
+    }
+    else {
+        return list_lookup(list->next, key);
+    }
 }
 
 
@@ -342,15 +348,16 @@ void print_map(Map *map)
 /* Adds a key-value pair to a map. */
 void map_add(Map *map, Hashable *key, Value *value)
 {
-    // FILL THIS IN!
+    int index = key->hash(key->key) % map->n;
+    map->lists[index] = prepend(key, value, map->lists[index]);
 }
 
 
 /* Looks up a key and returns the corresponding value, or NULL. */
 Value *map_lookup(Map *map, Hashable *key)
 {
-    // FILL THIS IN!
-    return NULL;
+    int index = key->hash(key->key) % map->n;
+    return list_lookup(map->lists[index], key);
 }
 
 
